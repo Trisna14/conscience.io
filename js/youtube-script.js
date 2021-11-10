ee905db7db3f17e9cef22a73a8d7e97890be6734
emde5 <trisna.tkjtamsisjetis14@gmail.com>
2021-11-10 22:02:21 +0700

    modifikasi youtube dengan vanilla js

--- js/youtube-script.js
+++ js/youtube-script.js
@@ -1,15 +1,18 @@
 const btn_youtubeShow = document.getElementsByClassName('video-youtube-vnjs')[0];
 const youtube_show = document.getElementsByClassName('video-youtube-show')[0];
+const youtube_i = document.getElementsByClassName('youtube-i')[0];
 // console.log(btn_youtubeShow);
 
 function youTubeShow () {
     // console.log('ok');
     youtube_show.classList.add('showY');
+    youtube_i.classList.add('showY');
     console.log(youtube_show);
 }
 
 function youTubeOut () {
     // console.log('ok');
     youtube_show.classList.remove('showY');
+    youtube_i.classList.remove('showY');
     console.log(youtube_show);
 }

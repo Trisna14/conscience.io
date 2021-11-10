7efe540edf7fd6c9a7c1806a75328aedd1e9aebf
emde5 <trisna.tkjtamsisjetis14@gmail.com>
2021-11-10 18:51:51 +0700

    menambahkan script vanilla js youtube btn

---
+++ js/youtube-script.js
@@ -0,0 +1,8 @@
+const btn_youtubeShow = document.getElementsByClassName('video-youtube-vnjs')[0];
+console.log(btn_youtubeShow);
+
+btn_youtubeShow.addEventListener ('click', (btn) => {
+
+    console.log(btn);
+    
+});

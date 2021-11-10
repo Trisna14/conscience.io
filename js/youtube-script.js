0adae6510f9544370e2bde54234b4e2c77d47f3f
emde5 <trisna.tkjtamsisjetis14@gmail.com>
2021-11-10 21:17:00 +0700

    manipulasi youtube dengan vanilla js

--- js/youtube-script.js
+++ js/youtube-script.js
@@ -1,8 +1,15 @@
 const btn_youtubeShow = document.getElementsByClassName('video-youtube-vnjs')[0];
-console.log(btn_youtubeShow);
+const youtube_show = document.getElementsByClassName('video-youtube-show')[0];
+// console.log(btn_youtubeShow);
 
-btn_youtubeShow.addEventListener ('click', (btn) => {
+function youTubeShow () {
+    // console.log('ok');
+    youtube_show.classList.add('showY');
+    console.log(youtube_show);
+}
 
-    console.log(btn);
-    
-});
+function youTubeOut () {
+    // console.log('ok');
+    youtube_show.classList.remove('showY');
+    console.log(youtube_show);
+}
